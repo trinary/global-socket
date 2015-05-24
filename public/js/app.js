c137312440829401c738ab27f3ca22966dd730d7
Erik Cunningham <djtrinary@gmail.com>
2015-05-23 18:58:53 -0600

    first take on orbit controls

--- public/js/app.js
+++ public/js/app.js
@@ -29,7 +29,6 @@ var socket = io.connect('http://127.0.0.1:7008/');
 var width = window.innerWidth,
     height = window.innerHeight;
 
-var rotation = [0, -Math.PI / 2];
 var markers = [];
 
 var scene = new THREE.Scene();
@@ -37,7 +36,7 @@ var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
 var duration = 90000;
 
-var origin = new THREE.Vector3(0,0,0);
+var origin = new THREE.Vector3(0, 0, 0);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
@@ -45,14 +44,39 @@ var geometry = new THREE.SphereGeometry(10, 64, 64);
 var material = new THREE.MeshPhongMaterial({ map: THREE.ImageUtils.loadTexture('/img/2_no_clouds_8k.jpg')});
 
 var light = new THREE.DirectionalLight(0xffffff);
-
 light.position.set(0, 15, 15).normalize();
+
+var light2 = new THREE.DirectionalLight(0xffffff);
+light2.position.set(0, -15, -15).normalize();
+
 scene.add(light);
+scene.add(light2);
 
 var globe = new THREE.Mesh(geometry, material);
 scene.add(globe);
 
 camera.position.z = 20;
+
+
+var render = function () {
+//  requestAnimationFrame(render);
+  var timer = Date.now() * 0.0001;
+
+
+  markers.forEach(function (m) {
+    var age = new Date() - m.start;
+    var ratio = (duration - age) / duration;
+    m.mesh.material.opacity = ratio;
+    if (m.mesh.material.opacity < 0) {
+      scene.remove(m.mesh);
+      m = undefined;
+    }
+  });
+
+  markers = markers.filter(function (m) { return m !== undefined; });
+
+  renderer.render(scene, camera);
+};
 
 function addTweet(tweet) {
   if (tweet.location && tweet.location.geo) {
@@ -64,7 +88,7 @@ function addTweet(tweet) {
     } else {
       color = 0xEDA228;
     }
-    var marker = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 1), new THREE.MeshBasicMaterial({transparent: true, color: color }));
+    var marker = new THREE.Mesh(new THREE.BoxGeometry(0.07, 0.07, 1 + ((Math.random() - 0.5) * 0.01)), new THREE.MeshBasicMaterial({transparent: true, color: color }));
     if (geo.type === 'Point') {
       pos = geo.coordinates;
     } else if (geo.type === 'Polygon') {
@@ -74,39 +98,19 @@ function addTweet(tweet) {
       ];
     }
     var coords = latLongToVector3(pos[0], pos[1], 10);
-    console.log(pos, coords);
     marker.position.fromArray(coords);
     marker.lookAt(origin);
     markers.push({mesh: marker, start: new Date()});
     scene.add(marker);
+    render();
   }
 }
 
-
-var render = function () {
-  requestAnimationFrame(render);
-  var timer = Date.now() * 0.0001;
-  var camerapos = [(Math.cos(timer) * 20), Math.sin(timer * 2) * 12, (Math.sin(timer) * 20)];
-
-  markers.forEach(function(m) {
-    var age = new Date() - m.start;
-    var ratio = (duration - age) / duration;
-    m.mesh.material.opacity = ratio;
-    if (m.mesh.material.opacity < 0) {
-      scene.remove(m.mesh);
-      m = undefined;
-    }
-  });
-
-  markers = markers.filter(function(m) { return m !== undefined;});
-
-  camera.position.fromArray(camerapos);
-  light.position.fromArray(camerapos);
-  camera.lookAt(origin);
-
-  renderer.render(scene, camera);
-};
 render();
+
+var controls = new THREE.OrbitControls(camera);
+controls.damping = 0.2;
+controls.addEventListener('change', render);
 
 document.body.appendChild(renderer.domElement);
 
